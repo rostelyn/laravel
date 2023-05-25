13f449bb2ece52b1a98f4758cb2d31a3d6720000
kitt-null <jl.enriquez.0530@gmail.com>
2023-05-24 19:16:34 -0800

    website

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacts Page</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">Landing</a>
+            <a href="">Home</a>
+            <a href="">Profile</a>
+            <a href="">Events</a>
+            <a href="">News</a>
+            <a href="">Contact us</a>
+        </nav>
+    </header>
+
+    <h1>Welcome to contact page</h1>
+
+</body>
+</html>
